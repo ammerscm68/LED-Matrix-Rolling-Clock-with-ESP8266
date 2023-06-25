774b1496f5718a918a611bcaea52ac5566b287cf
Mario <91007879+ammerscm68@users.noreply.github.com>
2023-06-25 11:49:21 +0200

    Add files via upload

---
+++ LEDMatrixClockV7.14/time.ino
@@ -0,0 +1,106 @@
+
+void getTimeLocal()
+{
+  time_t tT = now();
+  // local time
+  time_t tTlocal = CE.toLocal(tT);
+  w = weekday(tTlocal);
+  d = day(tTlocal);
+  mo = month(tTlocal);
+  ye = year(tTlocal);
+  h = hour(tTlocal);
+  m = minute(tTlocal);
+  s = second(tTlocal);
+  //localMillisAtUpdate = millis();
+  //client.stop();
+}
+
+// =======================================================================
+
+void getAnimClock(bool AnimWait) {
+  byte digPos[4]={1,8,17,25};
+  int digHt = 12;
+  int num = 4; 
+  int i;
+  if(del==0) {
+    del = digHt;
+    for(i=0; i<num; i++) digold[i] = dig[i];
+    dig[0] = h/10 ? h/10 : 10;
+    dig[1] = h%10;
+    dig[2] = m/10;
+    dig[3] = m%10;
+    for(i=0; i<num; i++)  digtrans[i] = (dig[i]==digold[i]) ? 0 : digHt;
+  } else
+    del--;
+  clr();
+  for(i=0; i<num; i++) {
+    if(digtrans[i]==0) {
+      dy=0;
+      showDigit(dig[i], digPos[i], dig6x8);
+    } else {
+      dy = digHt-digtrans[i];
+      showDigit(digold[i], digPos[i], dig6x8);
+      dy = -digtrans[i];
+      showDigit(dig[i], digPos[i], dig6x8);
+      digtrans[i]--;}}
+  dy=0;
+  setCol(15,dots ? B00100100 : 0);
+  // setCol(16,dots ? B00100100 : 0);
+  refreshAll();
+  if (AnimWait) {MyWaitLoop(ScrollClockTime);} // Scrollgeschwindigkeit der Uhrzeit
+}
+
+// =======================================================================
+
+int FormatedTime()
+{
+String fTime, fsh, fsm;  
+getTimeLocal(); // aktuelle Zeit holen 
+if (h < 10) {fsh = "0"+String(h);} else {fsh = String(h);}  
+if (m < 10) {fsm = "0"+String(m);} else {fsm = String(m);}  
+// if (s < 10) {fss = "0"+String(s);} else {fss = String(s);}  
+fTime =fsh+fsm;
+// Serial.println("Formated Time: "+fTime); // Only for Debuging 
+return fTime.toInt();
+}
+
+// =======================================================================
+
+/*
+// Australia Eastern Time Zone (Sydney, Melbourne)
+TimeChangeRule aEDT = {"AEDT", First, Sun, Oct, 2, 660};    // UTC + 11 hours
+TimeChangeRule aEST = {"AEST", First, Sun, Apr, 3, 600};    // UTC + 10 hours
+Timezone ausET(aEDT, aEST);
+
+// Moscow Standard Time (MSK, does not observe DST)
+TimeChangeRule msk = {"MSK", Last, Sun, Mar, 1, 180};
+Timezone tzMSK(msk);
+
+// Central European Time (Frankfurt, Paris)
+TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};     // Central European Summer Time
+TimeChangeRule CET = {"CET ", Last, Sun, Oct, 3, 60};       // Central European Standard Time
+Timezone CE(CEST, CET);
+
+// United Kingdom (London, Belfast)
+TimeChangeRule BST = {"BST", Last, Sun, Mar, 1, 60};        // British Summer Time
+TimeChangeRule GMT = {"GMT", Last, Sun, Oct, 2, 0};         // Standard Time
+Timezone UK(BST, GMT);
+
+// UTC
+TimeChangeRule utcRule = {"UTC", Last, Sun, Mar, 1, 0};     // UTC
+Timezone UTC(utcRule);
+
+// US Eastern Time Zone (New York, Detroit)
+TimeChangeRule usEDT = {"EDT", Second, Sun, Mar, 2, -240};  // Eastern Daylight Time = UTC - 4 hours
+TimeChangeRule usEST = {"EST", First, Sun, Nov, 2, -300};   // Eastern Standard Time = UTC - 5 hours
+Timezone usET(usEDT, usEST);
+
+// US Central Time Zone (Chicago, Houston)
+TimeChangeRule usCDT = {"CDT", Second, Sun, Mar, 2, -300};
+TimeChangeRule usCST = {"CST", First, Sun, Nov, 2, -360};
+Timezone usCT(usCDT, usCST);
+
+// US Mountain Time Zone (Denver, Salt Lake City)
+TimeChangeRule usMDT = {"MDT", Second, Sun, Mar, 2, -360};
+TimeChangeRule usMST = {"MST", First, Sun, Nov, 2, -420};
+Timezone usMT(usMDT, usMST);*/
